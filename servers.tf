77042ce700368756eab02046fe4a55e4a8887e44
Phillip Marlow <phillip@marlow1.com>
2017-12-01 15:20:56 -0500

    Add Terraform

---
+++ servers.tf
@@ -0,0 +1,53 @@
+#Declare variables to be used
+variable aws_access_key {}
+variable aws_secret_key {}
+
+#Configure provider
+provider "aws" {
+	access_key = "${var.aws_access_key}"
+	secret_key = "${var.aws_secret_key}"
+	region     = "us-east-1"
+}
+
+#Provision resources
+resource "aws_instance" "web" {
+	ami = "ami-55ef662f"
+	instance_type = "t2.micro"
+	key_name = "terraform-key"
+	timeouts {
+		create = "15m"
+	}
+	
+	provisioner "remote-exec" {
+		inline = [
+			"sudo mkdir /ansible",
+			"sudo chown ec2-user:ec2-user /ansible"
+		]
+		connection {
+			user = "ec2-user"
+			private_key = "${file("terraform-key.pem")}"
+		}
+	}
+	
+	provisioner "file" {
+		source = "ansible_playbooks/"
+		destination = "/ansible"
+		connection {
+			user = "ec2-user"
+			private_key = "${file("terraform-key.pem")}"
+		}
+	}
+	
+	provisioner "remote-exec" {
+		inline = [
+			"sudo pip install ansible",
+			"sudo echo 'SED_Dev_Box ansible_connection=local' >> /etc/ansible/hosts",
+			"ansible-playbook /ansible/sed.yml -i 'SED_Dev_Box,' -c local"
+		]
+		connection {
+			user = "ec2-user"
+			private_key = "${file("terraform-key.pem")}"
+			timeout = "15m"
+		}
+	}
+}
