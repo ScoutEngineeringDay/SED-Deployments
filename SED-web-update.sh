17642f25a54abfaa0a7c9bcddfdae81bc91a3dae
Phillip Marlow <phillip@marlow1.com>
2017-12-20 14:36:37 -0500

    Ensure docker runs on startup and web/database containers restart when needed.

--- SED-web-update.sh
+++ SED-web-update.sh
@@ -3,4 +3,4 @@ sudo docker stop sed_website
 sudo docker rm sed_website
 sudo docker rmi sed_website
 sudo docker build --tag sed_website:latest /vagrant/SED-Web-Application/
-sudo docker run -d --name sed_website --publish 8000:8000 --link sed_database:sed_database sed_website
+sudo docker run -d --name sed_website --restart always --publish 8000:8000 --link sed_database:sed_database sed_website
