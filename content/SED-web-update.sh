9552b2ca144e4f69a4736d19134816edb4f9090f
Phillip Marlow <phillip@marlow1.com>
2017-12-21 07:38:34 -0500

    Update paths for redeploy scripts.

--- content/SED-web-update.sh
+++ content/SED-web-update.sh
@@ -2,5 +2,5 @@
 sudo docker stop sed_website
 sudo docker rm sed_website
 sudo docker rmi sed_website
-sudo docker build --tag sed_website:latest /vagrant/SED-Web-Application/
+sudo docker build --tag sed_website:latest /vagrant/content/SED-Web-Application/
 sudo docker run -d --name sed_website --publish 8000:8000 --link sed_database:sed_database sed_website
