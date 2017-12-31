1cdbcc650761f19a9464dade88278575018fc8ad
Walter Hiranpat <tanuguitar@gmail.com>
2017-12-31 18:58:55 -0500

    have it print the logs after you update

--- content/SED-web-update.sh
+++ content/SED-web-update.sh
@@ -5,4 +5,4 @@ sudo docker rmi sed_website
 
 sudo docker build --tag sed_website:latest /ansible/SED-Web-Application/
 sudo docker run -d --name sed_website --restart always --publish 8000:8000 --link sed_database:sed_database sed_website
-
+sudo docker logs sed_website -f
