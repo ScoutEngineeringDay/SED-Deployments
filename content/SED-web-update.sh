8a6c6c035e3d19b2940f62a66402b29c08969a01
Walter Hiranpat <tanuguitar@gmail.com>
2017-12-31 18:14:10 -0500

    the directory update didn't get updated within the script

--- content/SED-web-update.sh
+++ content/SED-web-update.sh
@@ -3,6 +3,6 @@ sudo docker stop sed_website
 sudo docker rm sed_website
 sudo docker rmi sed_website
 
-sudo docker build --tag sed_website:latest /vagrant/SED-Web-Application/
+sudo docker build --tag sed_website:latest /ansible/SED-Web-Application/
 sudo docker run -d --name sed_website --restart always --publish 8000:8000 --link sed_database:sed_database sed_website
 
